b8df0740395a61f109da4b8454032252db154684
Jasper Robison <jasper.robison22@gmail.com>
2022-03-31 11:28:18 +1100

    update zip script

--- zip.sh
+++ zip.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-zip gctl.zip gctl install.sh
+rm -rf gctl-bin
+mkdir gctl-bin
+
+cp gctl gctl-bin/
+cp install.sh gctl-bin/
+
+rm gctl-bin.zip
+
+zip gctl-bin.zip gctl-bin
+
+rm -rf gctl-bin
